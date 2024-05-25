183a9a08152db99947bca49454d518b994793e8f
marceloanton <asus.rok@gmail.com>
2024-05-25 12:20:01 -0300

    Scroll arreglado

--- TPO_Final/js/menu.js
+++ TPO_Final/js/menu.js
@@ -3,3 +3,7 @@ function toggleMenu() {
     var menu = document.querySelector('.menu');
     menu.classList.toggle('show');
 }
+
+var btnMenu = document.querySelector('.btn-menu');
+btnMenu.addEventListener('click', toggleMenu);
+console.log(btnMenu);
