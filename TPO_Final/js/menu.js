9334e5afe9355f2488caeb639fb6c5632d76a8b5
marceloanton <asus.rok@gmail.com>
2024-05-25 13:57:47 -0300

    Arreglo del boton, y agregado ultimos detalles

--- TPO_Final/js/menu.js
+++ TPO_Final/js/menu.js
@@ -1,9 +1,5 @@
 /* Boton para mostrar y ocultar el menu */
 function toggleMenu() {
-    var menu = document.querySelector('.menu');
+    const menu = document.querySelector('.menu');
     menu.classList.toggle('show');
 }
-
-var btnMenu = document.querySelector('.btn-menu');
-btnMenu.addEventListener('click', toggleMenu);
-console.log(btnMenu);
