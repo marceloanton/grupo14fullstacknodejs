5e8eacfc89bd0dc03166ca11ac1b41ff49a713b1
marceloanton <asus.rok@gmail.com>
2024-07-10 22:06:31 -0300

    Cambios Vercel

--- 06_TPO_Final_NodeJS/public/resources/js/web.js
+++
@@ -1,8 +0,0 @@
-var footer = document.getElementById('footer');
-
-var contenidoFooter = `<div class="d-sm-flex justify-content-center justify-content-sm-between">
-                            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © Grupo 14</span>
-                            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="#" target="_blank">En un mundo ideal</a> somos todos ricos</span>
-                       </div>`;
-
-footer.innerHTML = contenidoFooter;
