25dd4f4798a171b4cd8eeac5963e0de90a7f8201
Marcelo Anton <asus.rok@gmail.com>
2024-07-11 18:57:06 -0300

    Delete 06_TPO_Final_NodeJS directory

--- 06_TPO_Final_NodeJS/public/resources/js/config.js
+++
@@ -1,6 +0,0 @@
-// configuración de nuestra api
-const config = {
-    apiUrl: 'http://localhost:3000/api'
-};
-
-export default config;
--- 06_TPO_Final_NodeJS/src/config/config.js
+++
@@ -1,8 +0,0 @@
-// Funcion para buscar un puerto disponible que nos de el OS
-function buscarPuertoDisponible() {
-    const server = require('http').createServer();
-    server.listen(0);
-    const port = server.address().port;
-    server.close();
-    return port;
-}
